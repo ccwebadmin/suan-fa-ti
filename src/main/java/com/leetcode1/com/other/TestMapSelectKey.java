501f270fdbcf17685fd9ba3a165ea35eb6ae3a00
caicheng <2423462300@qq.com>
2021-10-25 18:36:54 +0800

    改成maven项目

---
+++ src/main/java/com/leetcode1/com/other/TestMapSelectKey.java
@@ -0,0 +1,23 @@
+package com.leetcode1.com.other;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestMapSelectKey {
+    public static void main(String[] args) {
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("de1","1");
+//        System.out.println(map.get("de1"));
+//        System.out.println(map.get("de2"));
+        List<String> list =null;
+
+        for (int i = 0; list!=null&&i <list.size() ; i++) {
+
+
+//        for(String str:list){
+            System.out.println(list.get(i));
+        }
+    }
+}
---
+++ src/main/java/com/leetcode1/com/other/toJsonString.java
@@ -0,0 +1,19 @@
+package com.leetcode1.com.other;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+
+
+public class toJsonString {
+
+    public static void main(String[] args) {
+        JSONObject object = new JSONObject();
+
+        object.put("certno","429001198210210073");
+        object.put("psn_name","黄峰");
+        object.put("psn_cert_type","01");
+        System.out.println(JSON.toJSONString(object));
+    }
+
+}
